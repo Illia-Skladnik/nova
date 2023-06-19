6644734d392d0935dc477ff5b74ff08da43d61c2
Illia Skladnik <scladnik@gmail.com>
2023-06-19 17:56:07 +0300

    absolute paths images added

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,20 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <style scoped lang="scss">
   @import '@/assets/styles/_mixins.scss';
+  @import '@/assets/styles/variables.scss';
 
   .header {
+    height: #{$banner-height-mobile}px;
     position: relative;
+
+    @include onMobile {
+      height: #{$banner-height-desktop}px;
+    }
+
+
     &__picture {
       display: flex;
       justify-content: center;
     }
 
     &__image {
-      height: 667px;
+      height: #{$banner-height-mobile}px;
       position: absolute;
 
       @include onMobile {
-        height: 870px;
+        height: #{$banner-height-desktop}px;
       }
 
       @include onDesktop {
@@ -28,12 +36,13 @@
 <template>
   <header class="header">
     <picture class="header__picture">
-      <source media="(min-width: 375px)" srcset="@/assets/images/desktop/main.jpg">
-      <img class="header__image" src="@/assets/images/mobile/main.jpg" alt="banner">
+      <source media="(min-width: 375px)" :srcset="desktopBanner">
+      <img class="header__image" :src="mobileBanner" alt="banner">
     </picture>
   </header>
 </template>
 
 <script setup>
-
+  const mobileBanner = new URL('@/assets/images/mobile/main.jpg', import.meta.url);
+  const desktopBanner = new URL('@/assets/images/desktop/main.jpg', import.meta.url);
 </script>
