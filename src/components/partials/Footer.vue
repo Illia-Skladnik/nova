835137b68d2a376c95cec4fb3f1a1542c01092b2
Illia Skladnik <scladnik@gmail.com>
2023-06-20 12:25:24 +0300

    Footer added

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,12 +1,79 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <style scoped lang="scss">
+  @import '@/assets/styles/_mixins.scss';
+  @import '@/assets/styles/variables.scss';
   .footer {
-    
+    background-color: $dark-blue-color;
+    padding-top: 30px;
+
+    @include onDesktop {
+      padding-top: 50px;
+    }
+
+    &__separator {
+      background-color: $vivid-orange-color;
+      width: 100%;
+      height: 1px;
+
+      margin-bottom: 40px;
+
+      @include onDesktop {
+        margin-bottom: 70px;
+      }
+    }
+
+    &__logo {
+      display: block;
+      margin: 0 auto 42px;
+
+      @include onDesktop {
+        margin-bottom: 63px;
+      }
+    }
+
+    &__social {
+      margin: 0 auto 52px;
+
+      @include onDesktop {
+        margin-bottom: 63px;
+      }
+    }
+
+    &__copyright {
+      display: block;
+      margin: 0 auto;
+      width: max-content;
+      color: $white-color;
+      margin-bottom: 25px;
+
+      font-family: 'Poppins', sans-serif;
+      font-size: 14px;
+      font-weight: 300;
+      line-height: 24px;
+
+      @include onDesktop {
+        font-weight: 400;
+      }
+    }
   }
 </style>
 
 <template>
   <footer class="footer">
-    Footer
+    <div class="footer__separator"></div>
+    <Logo class="footer__logo"/>
+    <Socials class="footer__social"/>
+    <span class="footer__copyright">{{ copyright }}</span>
   </footer>
 </template>
+
+<script setup>
+  import Logo from './Logo.vue';
+  import Socials from './Socials.vue';
+  import { computed } from 'vue';
+
+  const copyright  = computed(()=>{
+    const date = (new Date()).getFullYear();
+    return `© ${date} novaenergy. All rights reserved`
+  })
+</script>
---
+++ src/components/partials/Socials.vue
@@ -0,0 +1,55 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<style scoped lang="scss">
+  @import '@/assets/styles/_mixins.scss';
+  .socials {
+    width: min-content;
+    display: flex;
+    justify-content: space-between;
+    width: 154px;
+
+    @include onDesktop {
+      width: 160px;
+    }
+
+    &__social {
+      background-position: center;
+      height: 25px;
+      width: 25px;
+    }
+
+    &__instagram {
+      background-image: url('@/assets/svg/instagram.svg');
+
+      &:hover {
+        background-image: url('@/assets/svg/instagramActive.svg');
+      }
+    }
+
+    &__facebook {
+      background-image: url('@/assets/svg/facebook.svg');
+
+      &:hover {
+        background-image: url('@/assets/svg/facebookActive.svg');
+      }
+    }
+
+    &__whatsapp {
+      background-image: url('@/assets/svg/whatsapp.svg');
+
+      &:hover {
+        background-image: url('@/assets/svg/whatsappActive.svg');
+      }
+    }
+  }
+</style>
+
+<template>
+  <div class="socials">
+    <div class="socials__social socials__instagram"></div>
+    <div class="socials__social socials__facebook"></div>
+    <div class="socials__social socials__whatsapp"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
