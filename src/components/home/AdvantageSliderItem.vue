56600a121a907ca476aef3e7104410c4c8ce3a38
Illia Skladnik <scladnik@gmail.com>
2023-06-26 09:58:58 +0300

    5th block carousel init

---
+++ src/components/home/AdvantageSliderItem.vue
@@ -0,0 +1,84 @@
+<style  scoped lang="scss">
+  @import '@/assets/styles/_mixins.scss';
+  @import '@/assets/styles/variables.scss';
+  .advantage {
+    width: 375px;
+    margin: 0 auto;
+    position: relative;
+    // height: 1500px!important;
+
+    &__picture {
+      // width: 375px;
+      // height: 375px;
+    }
+
+    &__image {
+      width: 375px;
+      height: 375px;
+    }
+
+    &__text-block {
+      position: absolute;
+      width: 335px;
+      height: 426px;
+      top: 250px;
+      left: 20px;
+
+      background-color: $dark-blue-color;
+      padding: 40px 24px 0 31px;
+    }
+
+    &__title {
+      color: $white-color;
+      font-family: 'Poppins', sans-serif;
+      font-size: 32px;
+      font-weight: 600;
+      line-height: 41px;
+    }
+
+    &__description {
+      display: block;
+      color: $white-color;
+      font-family: 'Poppins', sans-serif;
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 24px;
+      margin-bottom: 20px;
+    }
+
+    &__title-wrapper {
+      margin-bottom: 30px;
+    }
+  }
+</style>
+
+<template>
+  <div class="advantage">
+    <picture class="advantage__picture">
+      <source media="(min-width: 1440px)" :srcset="advantageDesk">
+      <img class="advantage__image" :src="advantageMob" alt="advantage">
+    </picture>
+
+    <div class="advantage__text-block">
+      <div class="advantage__title-wrapper">
+        <span class="advantage__title">{{ itemNumber }}. </span>
+        <span class="advantage__title">{{ advantage.title }}</span>
+      </div>
+
+      <span class="advantage__description">{{ advantage.description }}</span>
+      <img :src="arrowLeftWhite" alt="arrow">
+      <img :src="arrowRightGreen" alt="arrow">
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import arrowLeftWhite from '@/assets/svg/arrowLeftWhite.svg';
+  import arrowRightGreen from '@/assets/svg/arrowRightGreen.svg';
+  import { computed } from 'vue';
+
+  const itemNumber = computed(() => props.index + 1 );
+  const advantageMob = new URL(`/src/assets/images/mobile/${props.advantage.image}`, import.meta.url);
+  const advantageDesk = new URL(`/src/assets/images/desktop/${props.advantage.image}`, import.meta.url);
+  const props = defineProps(['advantage', 'index']);
+</script>
--- src/components/home/FifthBlock.vue
+++ src/components/home/FifthBlock.vue
@@ -1,15 +1,126 @@
 <style scoped lang="scss">
-  .five {
+  @import '@/assets/styles/_mixins.scss';
+  @import '@/assets/styles/variables.scss';
 
+  .five {
+    background-color: $dark-navy-blue-color;
+    padding-bottom: 30px;
+
+    @include onDesktop {
+      padding-bottom: 70px;
+    }
+
+    &__title {
+      display: block;
+      margin: 0 auto;
+      width: max-content;
+      padding: 70px 0 40px;
+
+      color: $white-color;
+      font-family: 'Poppins', sans-serif;
+      font-weight: 600;
+      font-size: 32px;
+      line-height: 41px;
+
+      @include onDesktop {
+        font-size: 56px;
+        line-height: 68px;
+        padding: 100px 0 80px;
+      }
+    }
+
+    &__offer-button {
+      display: block;
+      margin: 0 auto;
+    }
+
+    &__carousel {
+      margin-bottom: 40px;
+    }
+
+    &__slider {
+      padding-bottom: 40px;
+    }
   }
+
+  .carousel-divider {
+   border-bottom: 2px solid;
+   width: 33px;
+   border-color: rgba(255, 255, 255, 0.1);
+ }
+
+ .border-color-selected {
+   border-color: #FAA51A;
+ }
 </style>
 
 <template>
   <div class="five">
-    Fifth Block (with carousel)
+    <h2 class="five__title">Zalety polibutylenu</h2>
+
+    <v-carousel :show-arrows="false" hide-delimiters v-model="slider" class="five__carousel" height="675px">
+      <v-carousel-item
+        v-for="advantage, index in advantages"
+        :key="index"
+      >
+        <AdvantageSliderItem
+          :advantage="advantage"
+          :index="index"
+        />
+      </v-carousel-item>
+    </v-carousel>
+
+    <div class="px-16 d-flex justify-center five__slider">
+      <div v-for="(slide,index) in advantages"
+        :key="index"
+        :aria-details="slide.title"
+        :class="index === slider ? 'border-color-selected':''"
+        class="carousel-divider mx-1">
+      </div>
+    </div>
+
+    <OfferButtonVue class="five__offer-button"/>
   </div>
 </template>
 
 
 <script setup>
+  import OfferButtonVue from '../partials/OfferButton.vue';
+  import { ref } from 'vue';
+  import AdvantageSliderItem from './AdvantageSliderItem.vue';
+
+  const slider = ref(0);
+
+  const advantages = [
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+    {
+      title: 'Odporny',
+      description: 'Polibutylen jest wyjątkowo odporny na niskie temperatury. Nie pęka gdy zamarznie w nich woda, a instalację można montować nawet przy -15 st. C! Nie koroduje, nie reaguje z chemią i wytrzymuje uderzenia hydrauliczne aż do 36 barów przy 20 st C. Jest najbardziej trwałym tworzywem nawet po 50 latach użytkowania!',
+      image: `advantage1.png`,
+    },
+  ]
 </script>
